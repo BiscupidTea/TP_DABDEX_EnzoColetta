f9ebae6d8a8aafe0d4e4def38a584a7a2d7a7729
Enzo Coletta <coletta.enzo@yahoo.com.ar>
2023-08-03 20:09:43 -0300

    finish insert

--- TPIntegrador_DABDEX_EnzoColetta/main.cpp
+++ TPIntegrador_DABDEX_EnzoColetta/main.cpp
@@ -3,11 +3,32 @@
 
 using namespace std;
 
-void InsertData();
-void DeleteData();
-void UpdateData();
+void InsertData(MYSQL* conexion, MYSQL_ROW fila, MYSQL_RES* resultado);
+void DeleteData(MYSQL* conexion, MYSQL_ROW fila, MYSQL_RES* resultado);
+void UpdateData(MYSQL* conexion, MYSQL_ROW fila, MYSQL_RES* resultado);
 void SelectData(MYSQL* conexion, MYSQL_ROW fila, MYSQL_RES* resultado);
 
+struct alumno
+{
+	string nombre;
+	string apellido;
+	string fechaNacimiento;
+	string DNI;
+};
+
+struct curso
+{
+	string curso;
+	string division;
+	string descripcion;
+	string nivel;
+};
+
+struct materias
+{
+	string nombre;
+	string nivel;
+};
 
 int main()
 {
@@ -44,15 +65,15 @@ int main()
 			switch (userInput)
 			{
 			case 1:
-				InsertData();
+				InsertData(conexion, fila, resultado);
 				system("pause");
 				break;
 			case 2:
-				DeleteData();
+				DeleteData(conexion, fila, resultado);
 				system("pause");
 				break;
 			case 3:
-				UpdateData();
+				UpdateData(conexion, fila, resultado);
 				system("pause");
 				break;
 			case 4:
@@ -72,17 +93,171 @@ int main()
 	system("pause");
 }
 
-void InsertData()
+void InsertData(MYSQL* conexion, MYSQL_ROW fila, MYSQL_RES* resultado)
+{
+	int userInput;
+	int estado;
+	string insercion;
+	const char* query;
+	alumno nuevoAlumno;
+	curso nuevoCurso;
+	materias nuevaMateria;
+	string id1;
+	string id2;
+	string id3;
+
+	do
+	{
+		system("cls");
+		cout << R"(--Seleciona que quieres insertar--
+
+1.Alumnos
+2.Cursos 
+3.Materias
+4.Curso alumno
+5.Curso materia
+
+0.Volver
+)" << endl;
+
+		cin >> userInput;
+		system("cls");
+		switch (userInput)
+		{
+		case 1:
+		{
+			cout << "Ingresa el apellido del alumno" << endl;
+			cin >> nuevoAlumno.apellido;
+			cout << "Ingresa el nombre " << endl;
+			cin >> nuevoAlumno.nombre;
+			cout << "Ingresa el anio, mes, dia de la fecha de nacimiento, separa con - " << endl;
+			cin >> nuevoAlumno.fechaNacimiento;
+			cout << "Ingresa el dni" << endl;
+			cin >> nuevoAlumno.DNI;
+
+			insercion = "insert into alumnos (apellido, nombre, fechaNacimiento, dni) values ('" + nuevoAlumno.apellido + "','" + nuevoAlumno.nombre + "','" + nuevoAlumno.fechaNacimiento + "','" + nuevoAlumno.DNI + "')";
+
+			query = insercion.c_str();
+			estado = mysql_query(conexion, query);
+
+			if (!estado)
+			{
+				cout << "Alumno ingresado correctamente!" << endl;
+			}
+			else
+			{
+				cout << "ERROR al ingresar alumno" << endl;
+			}
+		}
+		case 2:
+		{
+			cout << "Ingresa el curso" << endl;
+			cin >> nuevoCurso.curso;
+			cout << "Ingresa la division " << endl;
+			cin >> nuevoCurso.division;
+			cout << "Ingresa una pequenia descripcion " << endl;
+			cin >> nuevoCurso.descripcion;
+			cout << "Ingresa el nivel" << endl;
+			cin >> nuevoCurso.nivel;
+
+			insercion = "insert into cursos (curso, division, descripcion, nivel) values ('" + nuevoCurso.curso + "','" + nuevoCurso.division + "','" + nuevoCurso.descripcion + "','" + nuevoCurso.nivel + "')";
+
+			query = insercion.c_str();
+			estado = mysql_query(conexion, query);
+
+			if (!estado)
+			{
+				cout << "Curso ingresado correctamente!" << endl;
+			}
+			else
+			{
+				cout << "ERROR al ingresar curso" << endl;
+			}
+		}
+			break;
+		case 3:
+		{
+			cout << "Ingresa la materia" << endl;
+			cin >> nuevaMateria.nombre;
+			cout << "Ingresa el nivel " << endl;
+			cin >> nuevaMateria.nivel;
+
+			insercion = "insert into materias (nombreMateria, nivel) values ('" + nuevaMateria.nombre + "','" + nuevaMateria.nivel + "')";
+
+			query = insercion.c_str();
+			estado = mysql_query(conexion, query);
+
+			if (!estado)
+			{
+				cout << "Materia ingresada correctamente!" << endl;
+			}
+			else
+			{
+				cout << "ERROR al ingresar materia" << endl;
+			}
+		}
+			break;
+		case 4:
+		{
+			cout << "Ingresa el id del curso" << endl;
+			cin >> id1;
+			cout << "Ingresa el id del alumno " << endl;
+			cin >> id2;
+			cout << "Ingresa el anio lectivo " << endl;
+			cin >> id3;
+
+			insercion = "insert into cursoAlumno (idCurso, idAlumno, anio_lectivo) values ('" + id1 + "','" + id2 + "','" + id3 + "')";
+
+			query = insercion.c_str();
+			estado = mysql_query(conexion, query);
+
+			if (!estado)
+			{
+				cout << "Curso alumno ingresado correctamente!" << endl;
+			}
+			else
+			{
+				cout << "ERROR al ingresar curso alumno" << endl;
+			}
+		}
+			break;
+		case 5:
+		{
+			cout << "Ingresa el id del curso" << endl;
+			cin >> id1;
+			cout << "Ingresa el id de la materia " << endl;
+			cin >> id2;
+			cout << "Ingresa el anio lectivo " << endl;
+			cin >> id3;
+
+			insercion = "insert into cursoMateria (idCurso, idMateria, anio_lectivo) values ('" + id1 + "','" + id2 + "','" + id3 + "')";
+
+			query = insercion.c_str();
+			estado = mysql_query(conexion, query);
+
+			if (!estado)
+			{
+				cout << "Curso materia ingresado correctamente!" << endl;
+			}
+			else
+			{
+				cout << "ERROR al ingresar curso materia" << endl;
+			}
+		}
+			break;
+		default:
+			break;
+		}
+		system("pause");
+	} while (userInput != 0);
+}
+
+void DeleteData(MYSQL* conexion, MYSQL_ROW fila, MYSQL_RES* resultado)
 {
 
 }
 
-void DeleteData()
-{
-
-}
-
-void UpdateData()
+void UpdateData(MYSQL* conexion, MYSQL_ROW fila, MYSQL_RES* resultado)
 {
 
 }
